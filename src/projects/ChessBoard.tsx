03467d38bb5cd1b3b6fe0bb05a67aa231fbb7209
kundan singh patel <kundansingh066@gmail.com>
2025-04-27 20:45:28 +0530

    chess board

--- src/projects/ChessBoard.tsx
+++ src/projects/ChessBoard.tsx
@@ -1,5 +1,53 @@
+import { useState } from "react"
+
 export default function ChessBoard() {
+    const [click, setClick] = useState(false)
+    const [i, setI] = useState(0)
+    const [j, setJ] = useState(0)
+    const style = {
+        width: '80px',
+        height: '80px',
+        border: '1px solid black',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: `${click ? 'red' : "white"}`
+    }
+    function handleClick(a: number, b: number) {
+        setI(a)
+        setJ(b)
+        setClick(true)
+        // e.currentTarget.style.backgroundColor = '#FEF3C6'
+
+    }
+
     return (
-        <h1>ChessBoard</h1>
+        <div className="flex flex-col justify-center items-center gap-15">
+            <h1 className="text-center text-4xl">ChessBoard</h1>
+            <div>
+                {Array.from({ length: 8 }, (_, a) => (
+                    <div key={a} style={{ display: 'flex' }}>
+                        {Array.from({ length: 8 }, (_: unknown, b: number) => (
+                            <div onClick={() => handleClick(a, b)} key={b}
+                                style={{
+                                    width: '80px',
+                                    height: '80px',
+                                    border: '1px solid black',
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    backgroundColor: `${click && (b - a === j - i || b + a === j + i) && a !== i && b !== j
+                                        ? 'red'
+                                        : a === i && b === j
+                                            ? "#FEF3C6"
+                                            : "white"}`
+                                }}>
+                                {(a + 1)} ,{(b + 1)}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
